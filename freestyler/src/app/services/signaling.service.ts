ce5cb32f62b920ec5e7f40ec091dc2042c08568f
Daniel lara <DANIEL931@bailliegifford.com>
2024-10-05 11:33:45 +0100

    sd

--- freestyler/src/app/services/signaling.service.ts
+++ freestyler/src/app/services/signaling.service.ts
@@ -2,15 +2,23 @@ import { Injectable } from '@angular/core';
 import { default as io, Socket } from 'socket.io-client'; // Importing default
 import { Observable } from 'rxjs';
 import { Message } from '../models/message';
+import { AuthService } from './auth.service';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class SignalingService {
-  private socket: any; // Updated the type
+  private socket: any;
 
-  constructor() {
-    this.socket = io('http://localhost:3000'); // Make sure the server URL is correct
+  constructor(private authService: AuthService) {
+    const token = this.authService.getToken();
+
+    this.socket = io('http://localhost:3000', {
+      auth: {
+        token: `Bearer ${token}`,
+      }
+    });
   }
 
   sendMessage(tabId: string, message: Message): void {
---
+++ freestyler/src/app/services/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private apiUrl = 'http://localhost:3000/users'; // Adjust the URL as needed
+
+  constructor(private http: HttpClient) {}
+
+  // Get all users
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl);
+  }
+
+  // Get a user by ID
+  getUserById(id: string): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/${id}`);
+  }
+}
